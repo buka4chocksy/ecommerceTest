7ad3571d2192c09ec1cb20bd9edd2fcd0d68d7e3
buka4chocksy <buka4chocksy@gmail.com>
2020-01-25 23:27:10 +0100

    added deleteCategoery,updateCategoery , getById routes

---
+++ app/routes/categoryRoutes.js
@@ -0,0 +1,15 @@
+var categoryController = require('../Controller/categoryController');
+//var middleware = require('../middleware/AuthMiddleware');
+var router = require('express').Router();
+
+module.exports = function () {
+    const catCtrl = new categoryController();
+    router.post('/',  catCtrl.create);
+    router.get('/',  catCtrl.getAll);
+    router.get('/single',  catCtrl.getById);
+    router.put('/',  catCtrl.updateCategoery);
+    router.delete('/',  catCtrl.deleteCategoery);
+  
+
+    return router;
+}
