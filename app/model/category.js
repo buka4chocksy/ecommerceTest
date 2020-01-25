9cf25ada3c72427fd0bbda98ca586394f72346ff
buka4chocksy <buka4chocksy@gmail.com>
2020-01-25 23:11:40 +0100

    created category model

--- app/model/category.js
+++ app/model/category.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 var schema = mongoose.Schema;
 var categorySchema = new schema({
-    Name:{type:String}
+    name:{type:String}
 })
 
 module.exports = mongoose.model('category' , categorySchema);
