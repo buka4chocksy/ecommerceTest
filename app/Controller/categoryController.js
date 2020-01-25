976f27ff448958e3df22478a875995ca850482a3
buka4chocksy <buka4chocksy@gmail.com>
2020-01-25 23:26:24 +0100

    added get category by id , get all category , update category and delete category

--- app/Controller/categoryController.js
+++ app/Controller/categoryController.js
@@ -16,4 +16,28 @@ module.exports = categoryController = function(){
          res.json(err);
      })
      }
+
+     this.getById = (req,res)=>{
+        service.getCategoryById(req.query.id).then(data =>{
+         res.json(data);
+        }).catch((err) => {
+         res.json(err);
+     })
+     }
+
+     this.updateCategoery = (req,res)=>{
+        service.UpdateCategory(req.query.id ,req.body).then(data =>{
+         res.json(data);
+        }).catch((err) => {
+         res.json(err);
+     })
+     }
+
+     this.deleteCategoery = (req,res)=>{
+        service.deleteCategory(req.query.id ).then(data =>{
+         res.json(data);
+        }).catch((err) => {
+         res.json(err);
+     })
+     }
 }
