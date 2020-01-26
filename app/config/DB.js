469ecd0dd8cb399ec1046b7aa5479d81dd9fcbf2
buka4chocksy <buka4chocksy@gmail.com>
2020-01-26 16:40:55 +0100

    modified database  connection to live db

--- app/config/DB.js
+++ app/config/DB.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const uri = process.env.DB_LOCAL;
+const uri = process.env.DB_URI;
 //const uri = process.env.NODE_ENV == 'prod' ? process.env.DB_PROD : process.env.DB_LOCAL;
 
 module.exports = function init() {
