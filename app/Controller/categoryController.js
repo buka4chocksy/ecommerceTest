b11b9cf2be74d3ee31ba46e0685d5c095ad4cf76
buka4chocksy <buka4chocksy@gmail.com>
2020-01-26 16:04:43 +0100

    ran a CRUD method for the category controller

--- app/Controller/categoryController.js
+++ app/Controller/categoryController.js
@@ -1,43 +1,58 @@
-var service = require('../service/categoryService')
+var service = require("../service/categoryService");
 
-module.exports = categoryController = function(){
-    this.create = (req,res)=>{
-       service.createCategory(req.body).then(data =>{
+module.exports = categoryController = function() {
+  this.create = (req, res) => {
+    service
+      .createCategory(req.body)
+      .then(data => {
         res.json(data);
-       }).catch((err) => {
+      })
+      .catch(err => {
         res.json(err);
-    })
-    }
+      });
+  };
 
-    this.getAll = (req,res)=>{
-        service.getAllCategory({}).then(data =>{
-         res.json(data);
-        }).catch((err) => {
-         res.json(err);
-     })
-     }
+  this.getAll = (req, res) => {
+    service
+      .getAllCategory({})
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
 
-     this.getById = (req,res)=>{
-        service.getCategoryById(req.query.id).then(data =>{
-         res.json(data);
-        }).catch((err) => {
-         res.json(err);
-     })
-     }
+  this.getById = (req, res) => {
+    service
+      .getCategoryById(req.query.id)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
 
-     this.updateCategoery = (req,res)=>{
-        service.UpdateCategory(req.query.id ,req.body).then(data =>{
-         res.json(data);
-        }).catch((err) => {
-         res.json(err);
-     })
-     }
+  this.updateCategoery = (req, res) => {
+    service
+      .UpdateCategory(req.query.id, req.body)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
 
-     this.deleteCategoery = (req,res)=>{
-        service.deleteCategory(req.query.id ).then(data =>{
-         res.json(data);
-        }).catch((err) => {
-         res.json(err);
-     })
-     }
-}
+  this.deleteCategoery = (req, res) => {
+    service
+      .deleteCategory(req.query.id)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  };
+};
